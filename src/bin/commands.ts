133fbdd4d148ddb89f5822c1b8f9fdcaf3857c1c
dchicchon <danielchicchon@gmail.com>
2021-11-03 15:48:24 -0400

    update the commands

--- src/bin/commands.ts
+++ src/bin/commands.ts
@@ -1,10 +1,27 @@
 #! /usr/bin/env node
+
+import { readFileSync, writeFile, } from 'fs'
+import { exec, echo, cd, cp, cat, ShellString, sort } from 'shelljs'
+
+
+// what do we need to do, we need to read the current
+// directory that we are in, look for src folder.
+// Look for the App.js file and use that as our root for
+// our project. Then we can ask the user to select all the files
+//
+const log = console.log
 export module Commands {
     export const exportApp = () => {
         try {
-            console.log("Exporting the app here!")
+            log("Exporting the app here!")
+            console.log(process.cwd()) // print the root of where we are in'
+            // lets create a folder called export_app
+            exec('mkdir -p export_app') // also check if there is a folder called export_app
+            // look for a src folder
+
+
         } catch (err) {
-            console.log("There was an error in exporting your app")
+            log("There was an error in exporting your app")
         }
     }
 }
